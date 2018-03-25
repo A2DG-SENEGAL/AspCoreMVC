2733a31528aba999e4dfe3b5ca792cc7611b4a4d
Birante SY <birantesy@gmail.com>
2018-03-25 12:48:29 +0000

    Project Initial

---
+++ SimpleApplication/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SimpleApplication.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SimpleApplication
+{
+    public class HomeController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+
+            var personne = new Personne { Nom = "A2DG" , Prenom = "Senegal" , Age = 1};
+
+            return View(personne);
+        }
+
+       
+    }
+}
---
+++ SimpleApplication/Models/Personne.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace SimpleApplication.Models
+{
+    public class Personne
+    {
+
+        public string Nom
+        {
+            get;
+            set;
+        }
+
+        public string Prenom
+        {
+            get;
+            set;
+        }
+
+        public int Age
+        {
+            get;
+            set;
+        }
+       
+    }
+}
